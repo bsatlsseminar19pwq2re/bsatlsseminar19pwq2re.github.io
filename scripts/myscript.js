7c959c75edfa714a32418334adc8619540315b36
bsatlsseminar19pwq2re <66995768+bsatlsseminar19pwq2re@users.noreply.github.com>
2020-06-16 10:51:17 +0200

    Update myscript.js

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,5 +1,7 @@
 (function() {
-  console.log(document.getElementsByTagName("body")[0]);
-  document.getElementsByTagName("body")[0].style.backgroundColor='red';
-  document.getElementsByTagName("body")[0].innerHTML='pwned';
+  window.addEventListener('load', (event) => {
+    console.log(document.getElementsByTagName("body")[0]);
+    document.getElementsByTagName("body")[0].style.backgroundColor='red';
+    document.getElementsByTagName("body")[0].innerHTML='pwned';
+  });
 })();
