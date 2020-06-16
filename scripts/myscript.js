d03ff68df7d1f11f12ebd92b74482e8792934671
bsatlsseminar19pwq2re <66995768+bsatlsseminar19pwq2re@users.noreply.github.com>
2020-06-16 10:47:18 +0200

    Update myscript.js

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,4 +1,4 @@
 (function() {
-  document.getElementsByTagName("BODY")[0].style.background = "red";
+  document.getElementsByTagName("BODY")[0].style.backgroundColor='red';
   document.getElementsByTagName("BODY")[0].innerHTML='pwned';
 })();
